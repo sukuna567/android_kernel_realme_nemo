0cc6509185d3514ea5cc84c9df2cf0fe31bae053
sukuna567 <bhudev12364@gmail.com>
2026-07-12 12:03:33 +0000

    kernel: Optimize schedutil and swappiness for multitasking
    - Force cpufreq_schedutil up_rate_limit_us to 500us for instant
      frequency ramp-up during UI touch/scroll events to eliminate stutter.
    - Force down_rate_limit_us to 20000us so frequencies don't drop too
      quickly between micro-workloads.
    - Increase vm_swappiness to 100 to maximize ZRAM efficiency,
      drastically improving background app retention for heavy multitasking.

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -184,7 +184,8 @@ unsigned long sysctl_clean_min_kbytes __read_mostly = CONFIG_CLEAN_MIN_KBYTES;
 /*
  * From 0 .. 100.  Higher means more swappy.
  */
-int vm_swappiness = 60;
+/* OVERRIDE: Increase swappiness to 100 to maximize ZRAM efficiency for heavy multitasking */
+int vm_swappiness = 100;
 /*
  * The total number of pages which are beyond the high watermark within all
  * zones.
